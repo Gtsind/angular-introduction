905ae63518cd75e83e8cf8a7a44b86e30e74dd4f
Gtsind <tsindarisg@gmail.com>
2025-05-18 15:24:25 +0300

    New service added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { UserRegistrationComponent } from './components/user-registration/user-registration.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'user-registration-example', component: UserRegistrationComponent }
+];
